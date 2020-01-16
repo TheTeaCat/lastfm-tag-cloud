4b435189157cd2926c9fccf49c6a0c229e002dee
Joshua O'sullivan <joshua.r.osullivan@gmail.com>
2020-01-16 22:15:39 +0000

    Style control panel (addresses #1)

--- src/components/CollapseButton.vue
+++ src/components/CollapseButton.vue
@@ -15,9 +15,6 @@
         width:1.5em;
         height:1.5em;
 
-        font-family:'Courier New', Courier, monospace;
-        font-size:11pt;
-
         background:#fff;
         border-style:dashed;
         border-width:1px;
--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,23 +1,31 @@
 <template>
-    <div>
-        Last.fm Username:
-        <input v-model="username"/>
+    <div id="container">
+        <div class="option">
+            <span>Last.fm Username:</span>
+            <input v-model="username"/>
+        </div>
 
-        <button v-on:click="$emit('generate')">Load Data</button>
+        <div class="option">
+            <span>Time Period:</span>
+            <select v-model="period.selected">
+                <option v-for="option in period.options" v-bind:key="option.value" v-bind:value="option.value">
+                    {{ option.text }}
+                </option>
+            </select>
+        </div>
 
-        Time Period:
-        <select v-model="period.selected">
-            <option v-for="option in period.options" v-bind:key="option.value" v-bind:value="option.value">
-                {{ option.text }}
-            </option>
-        </select>
+        <div class="option">
+            <span>Max Artists:</span>
+            <select v-model="max_artists.selected">
+                <option v-for="option in max_artists.options" v-bind:key="option.value" v-bind:value="option.value">
+                    {{ option.text }}
+                </option>
+            </select>
+        </div>
 
-        Max Artists:
-        <select v-model="max_artists.selected">
-            <option v-for="option in max_artists.options" v-bind:key="option.value" v-bind:value="option.value">
-                {{ option.text }}
-            </option>
-        </select>
+        <div class="option" id="button-container">
+            <button v-on:click="$emit('generate')">Load Data</button>
+        </div>
     </div>
 </template>
 
@@ -26,3 +34,44 @@
         props: ['username','period','max_artists']
     }
 </script>
+
+<style scoped>
+    #container {
+        border-width:1px;
+        border-style:solid;
+        margin:1vw 0 1vw 0;
+        padding:0.6vw 1vw 0.5vw 0.5vw;
+
+        display: flex;
+        flex-wrap:wrap;
+    }
+    @media (orientation: portrait) {
+        #container { 
+            padding:1vw 1.5vw 1vw 1vw;
+            flex-direction:row;
+        }
+    }
+
+    .option { display:inline; }
+    @media (orientation: portrait) {
+        .option { 
+            flex-basis:60%;
+            flex-grow:1;
+            margin:0.5vw 0 0.5vw 0;
+        }
+    }
+
+    button { margin:0 0 0 1vw; }
+    @media (orientation: portrait) {
+        #button-container { 
+            flex-basis:30%;
+            align-self:flex-end;
+
+            display:flex;
+            justify-content:flex-end;
+        }
+    }
+
+    span { margin:0 0.5vw 0 1vw; }
+    span:first-child { margin:0 0.5vw 0 0.5vw; }
+</style>
