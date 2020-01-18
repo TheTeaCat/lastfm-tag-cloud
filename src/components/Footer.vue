ce54dbf3369548ab30ac6e9bcb6ef39c6b96eb20
TheTeaCat <joshua.r.osullivan@gmail.com>
2020-01-18 18:46:58 +0000

    Fix typo in footer href

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="section-container">
         <ul>
             <li><a href="https://www.last.fm/user/theteacat">My Last.Fml</a></li>
-            <li><a href="https://github.com/PedantiCat/tag-cloudd">Github</a></li>
+            <li><a href="https://github.com/PedantiCat/tag-cloud">Github</a></li>
             <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#how-are-the-tags-chosen--scaled">How Are The Tags Chosen?</a></li>
         </ul>
     </footer>
