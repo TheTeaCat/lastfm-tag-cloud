fd5e87734fc8691699aa3aca004dcda3d14e9771
TheTeaCat <joshua.r.osullivan@gmail.com>
2020-01-21 15:18:57 +0000

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,8 @@
             <li><a href="https://www.last.fm/user/theteacat">My Last.Fml</a></li>
             <li><a href="https://github.com/PedantiCat/tag-cloud">Github</a> -  Star This!</li>
             <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#how-are-the-tags-chosen--scaled">How Are The Tags Chosen?</a></li>
-        </ul>
+             <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#what-does-the-tag-filter-do">What Does The Tag Filter Do?</a></li>
+       </ul>
     </footer>
 </template>
 
