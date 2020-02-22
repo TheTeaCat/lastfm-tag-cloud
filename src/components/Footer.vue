16ce99e561528f6ea2c3d80c39586c522612ab41
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-02-22 00:40:41 +0000

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="https://www.last.fm/user/theteacat">My Last.Fml</a></li>
             <li><a href="https://github.com/PedantiCat/tag-cloud">Github</a> -  Star This!</li>
-            <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#how-are-the-tags-chosen--scaled">How Are The Tags Chosen?</a></li>
+            <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#how-are-the-tags-chosen--scaled">How Are The Tags Chosen & Scaled?</a></li>
              <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#what-does-the-tag-filter-do">What Does The Tag Filter Do?</a></li>
        </ul>
     </footer>
