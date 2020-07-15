5033c21fcb8d18fc660d1460bc3948c3516dca46
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-07-15 22:04:42 +0100

    Restore spinner

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,3 +1,20 @@
 <template>
     <div></div>
 </template>
+
+<style scoped>
+@keyframes spinner {
+    0%,100%{content:"|"}
+    25%{content:"/"}
+    50%{content:"-"}
+    75%{content:"\\"}
+}
+div {
+    font-family:'Courier New', Courier, monospace;
+    display:inline-block;
+}
+div::after {
+    content:"";
+    animation: spinner 1s steps(4,end) infinite;
+}
+</style>
