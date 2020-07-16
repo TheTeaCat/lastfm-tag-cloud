4ce466a7ee8e98521e2f13dd0325835b95845651
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-07-16 18:11:27 +0100

    Delete spinner component

--- src/components/ResultTitle.vue
+++ src/components/ResultTitle.vue
@@ -1,23 +1,17 @@
 <template>
   <h2 v-if="fetchedData && state == undefined">
     <a :href="'https://www.last.fm/user/'+username">
-        {{ username }}
-    </a>{{ artistCount > 0 ? 
-                (username[username.length-1].toLowerCase() == "s" ? "'" : "'s") 
-                : "" }}
-
+      {{ username }}</a>{{ artistCount > 0 ? (username[username.length-1].toLowerCase() == "s" ? "'" : "'s") : "" }}
     {{ (artistCount > 0 ? 
             "tag cloud based upon their top " + artistCount + " artists "
             : "hasn't listened to anything ")
-
         + {'7day':'over the last 7 days',
         '1month':'over the last month',
         '3month':'over the last 3 months',
         '6month':'over the last 6 months',
         '12month':'over the last year',
         'overall':'overall'}[timePeriod]
-
-      + (artistCount > 0 ? ':' : "")
+        + (artistCount > 0 ? ':' : "")
     }}
   </h2>            
   <h2 v-else-if="state == undefined && error != undefined">
@@ -26,17 +20,29 @@
       {{ error }}
   </h2>
   <h2 v-else-if="state != undefined">
-      <spinner/> {{ state }}
+      <div class="spinner"></div> {{ state }}
   </h2>
 </template>
 
 <script>
-import Spinner from "./Spinner.vue"
-
 export default {
-  components: {
-    Spinner
-  },
   props: ['fetchedData', 'state', 'username', 'artistCount', 'timePeriod', 'error'],
 }
 </script>
+
+<style lang="scss" scoped>
+@keyframes spinner {
+    0%,100%{content:"|"}
+    25%{content:"/"}
+    50%{content:"-"}
+    75%{content:"\\"}
+}
+.spinner {
+    font-family:'Courier New', Courier, monospace;
+    display:inline-block;
+}
+.spinner::after {
+    content:"";
+    animation: spinner 1s steps(4,end) infinite;
+}
+</style>
