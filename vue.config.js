23de3f51efe06378a5b6fac6db8aa0c607e5231e
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-07-16 18:58:04 +0100

    Lint

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        // @/ is an alias to src/
-        // so this assumes you have a file named `src/variables.scss`
         additionalData: `
         @import '@/assets/scss/reset.scss';
         @import "@/assets/scss/_vars.scss";
