c4d67e95a09c49bf954801940623e381d0e4e79d
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-08-15 15:04:08 +0100

    Add "leave me a shout" notice to footer

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
 
     <footer>
       <ul>
-        <li><a href="https://www.last.fm/user/theteacat">My Last.Fml</a></li>
+        <li><a href="https://www.last.fm/user/theteacat">My Last.Fm</a> - Leave me a shout!</li>
         <li><a href="https://github.com/PedantiCat/tag-cloud">Github</a> -  Star This!</li>
         <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#how-are-the-tags-chosen--scaled">How Are The Tags Chosen & Scaled?</a></li>
         <li><a href="https://github.com/TheTeaCat/lastfm-tag-cloud#what-does-the-tag-filter-do">What Does The Tag Filter Do?</a></li>
